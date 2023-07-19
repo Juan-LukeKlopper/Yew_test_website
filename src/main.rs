adfc6194ea746ff86103f33408ca38ebf9356f23
Juan-LukeKlopper <jlklopper@protonmail.com>
2023-07-19 13:15:28 +0200

    feat: Create basic main.rs to generate HTML

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use yew::prelude::*;
+
+#[function_component]
+fn App() -> Html {
+    let header = "Hello world".to_string();
+    let counter = use_state(|| 0 );
+    let onclick = {
+        let counter = counter.clone();
+        move |_| {
+            let value = *counter + 1;
+            counter.set(value);
+        }
+    };
+
+    html! {
+        <div>
+            <h1>{ header }</h1>
+            <button {onclick}>{ "+1" }</button>
+            <p>{ *counter }</p>
+        </div>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::Renderer::<App>::new().render();
 }
