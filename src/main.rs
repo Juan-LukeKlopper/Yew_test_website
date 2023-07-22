1b8c55ff15b4eef6c232952e905e815e6255f05c
Juan-LukeKlopper <jlklopper@protonmail.com>
2023-07-22 11:27:43 +0200

    fix: Make background clear and make it's position reactive

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,27 @@ use stylist::{yew::styled_component, style };
 
 #[styled_component]
 fn App() -> Html {
-    let header = "Hello world".to_string();
+    let header = "Intercosmic Labs".to_string();
+    let footer = "Please Stand by as we're getting ready for Blast off!".to_string();
 
     let stylesheet = style!(
             r#"
-                background-color: blue; 
-            "#
+                background-color: None;
+                padding-top: min(5%);
+                padding-top: max(12.5%);
+                padding-right: 7.5%;
+                padding-bottom: 2%;
+                padding-left: 7.5%;
+                "#
         ).expect("Failed to load stylesheet");
 
     html! {
-        <html class={stylesheet}>
+        <div class={stylesheet}>
             <div>
                 <h1>{ header }</h1>
-                <p>{ "This is a paragraph" }</p>
+                <p>{ footer }</p>
             </div>
-        </html>
+        </div>
     }
 }
 
